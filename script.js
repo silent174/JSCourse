30e7cd689376b32f5f2eb4278c20dd521a3853e0
Alekseev Kirill <alekseev_kirill_a@mail.ru>
2020-10-13 22:58:35 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+ 
+// const category = 'toys';
+// console.log(`https://mysite/${category}/5`);
+
+// const name = prompt("Как тебя зовут?", "");
+// const age = prompt(`Сколько тебе лет ${name}?`,"");
+// const city = prompt(`Из какого ты города. ${name}?`,"");
+// alert(`Добро пожаловать, ${name},  тебе ${age} лет и ты из города ${city}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const ischecked = true,
+      isclose = false;
+
+console.log(ischecked || !isclose);
+
+
