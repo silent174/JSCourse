4fa6c3a2815e4b136ab5c949871741780fe631c7
Kirill Alekseev <alekseev_kirill_a@mail.ru>
2020-10-14 16:27:00 +0500

    Циклы

--- script.js
+++ script.js
@@ -20,15 +20,35 @@
 
 // console.log(personalMovieDB);
 
-const num = 70;
+// const num = 70;
 
-switch (num){
-    case 15: console.log("num os not 15");
-    break;
-    case 22: console.log("num is not 22");
-    break;
-    case 10: console.log("num is 10. Congratilations!");
-    break;
-    default: console.log("there is not right number");
-    break;
+// switch (num){
+//     case 15: console.log("num os not 15");
+//     break;
+//     case 22: console.log("num is not 22");
+//     break;
+//     case 10: console.log("num is 10. Congratilations!");
+//     break; 
+//     default: console.log("there is not right number");
+//     break;
+// }
+let num = 10;
+
+// while(num <= 24){
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++
+// }
+// while (num < 20);
+
+for (let i = 1; i < 8; i++){
+    if (i === 6 ){
+        // break;
+        continue;
+    }
+    console.log(i);
 }
