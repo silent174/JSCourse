d0a7e0f2a8498465c76ec2590d04a31328c16bd7
Kirill Alekseev <alekseev_kirill_a@mail.ru>
2020-10-14 15:51:55 +0500

    else if, Switch Case

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 "use strict";
  
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    private:false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     private:false
+// };
 
-let a = prompt("Один из последних просмотренных фильмов? ", ""),
-    b = prompt("На сколько оцените его? ", ""), 
-    c = prompt("Один из последних просмотренных фильмов? ", ""),
-    d = prompt("На сколько оцените его? ", "");
+// let a = prompt("Один из последних просмотренных фильмов? ", ""),
+//     b = prompt("На сколько оцените его? ", ""), 
+//     c = prompt("Один из последних просмотренных фильмов? ", ""),
+//     d = prompt("На сколько оцените его? ", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+const num = 70;
+
+switch (num){
+    case 15: console.log("num os not 15");
+    break;
+    case 22: console.log("num is not 22");
+    break;
+    case 10: console.log("num is 10. Congratilations!");
+    break;
+    default: console.log("there is not right number");
+    break;
+}
